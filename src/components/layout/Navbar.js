a29a07d563b95448b53146bb010a9285a88f49c6
Nishant Chavda <nishantchavda36@gmail.com>
2022-06-11 22:26:41 -0400

    intial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+class Navbar extends Component {
+ 
+ //default props 
+  static defaultProps = {
+   title: 'Github Finder',
+   icon: 'fab fa-github'
+ }
+ 
+//PropTypes for type checking 
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+     <nav className='navbar bg-primary'>  
+       <h1>
+        <i className={this.props.icon}/> {this.props.title}
+         </h1>
+     </nav>
+    )
+  }
+}
+
+export default Navbar
---
+++ src/components/users/Useritems.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Useritems extends Component {
+
+
+  
+  render() {
+    return (
+      <div>
+       
+      </div>
+    )
+  }
+}
+
+export default Useritems
